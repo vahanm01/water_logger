ededfbc9cdefcd4d75a593661f731a0e38b2fe1e
Vahan Mouradian <vahanmouradian@outlook.com>
2020-04-21 19:39:07 -0700

    new processing script

--- raw_heffect_counter.py
+++ raw_heffect_counter.py
@@ -1,5 +1,5 @@
 
-
+from random import randint
 from sqlalchemy import create_engine
 import psycopg2
 
@@ -25,42 +25,37 @@ file_log="/home/pi/Desktop/Water_Sensor/water_log.csv"
 
 
 
-
-test_dict={}
-test_dict={str(datetime.datetime.now()):15}
-test_dict[str(datetime.datetime.now())]=15
-test_dict[str(datetime.datetime.now())]=20
+global raw_dict
+raw_dict={}
 
 
-test_dict = pd.DataFrame(list(test_dict.items()), columns=['record_date', 'total_count'])
 
 #global logger
 #logger=[]
-x=1
+
 
 
 while True:
 
-   if GPIO.event_detected(FLOW_SENSOR):
-       count = count + 1
-      
-  engine=create_engine('postgresql://beef:Felicia2020#@water-logger.cmoec5ph6uhr.us-east-1.rds.amazonaws.com:5432/raw_logs')
-  conn = engine.raw_connection()
-  cur = conn.cursor()
-  output=io.StringIO()
-  test_dict.to_csv(output, sep='\t', header=False, index=False, doublequote=False, escapechar='\\')
-  output.seek(0)
-  conents=output.getvalue()
-  cur.copy_from(output, 'raw_effect_counts', null="")
-  conn.commit()
-  count = 0
-  test_dict={}
+   #if GPIO.event_detected(FLOW_SENSOR):
+   count = count + 1
+   
 
-   else:
+   if count >= 50000:
+      raw_dict={str(datetime.datetime.now()):randint(0,100000)}
+      raw_dict = pd.DataFrame(list(raw_dict.items()), columns=['record_date', 'total_count'])
+      engine=create_engine('postgresql://beef:Felicia2020#@water-logger.cmoec5ph6uhr.us-east-1.rds.amazonaws.com:5432/raw_logs')
+      conn = engine.raw_connection()
+      cur = conn.cursor()
+      output=io.StringIO()
+      raw_dict.to_csv(output, sep='\t', header=False, index=False, doublequote=False, escapechar='\\')
+      output.seek(0)
+      conents=output.getvalue()
+      cur.copy_from(output, 'raw_effect_counts', null="")
+      conn.commit()
+      count = 0
+      raw_dict={}
 
-      count=0
-      test_dict={}
-      
       
       
       
