0cebf0b098d410794e79aed6462d55795eaeca13
Vahan Mouradian <vahanmouradian@outlook.com>
2020-07-17 08:03:21 -0700

    added detector

--- detector.py
+++ detector.py
@@ -17,12 +17,12 @@ while True:
 
      pulse_dict={'timestamp':str(datetime.datetime.now()), 'flow':str('True')}
      
-     with open('C:/Users/vahan/water_logger/detector.json', 'w') as fp:
+     with open('detector.json', 'w') as fp:
          json.dump(pulse_dict, fp)
 
   if GPIO.event_detected(FLOW_SENSOR)==False:
 
      pulse_dict={'timestamp':str(datetime.datetime.now()), 'flow':str('False')}
      
-     with open('C:/Users/vahan/water_logger/detector.json', 'w') as fp:
+     with open('detector.json', 'w') as fp:
          json.dump(pulse_dict, fp)  
