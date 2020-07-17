0bebde5d5da44f0a8c975035ccc8c32e0cdfd4e4
Vahan Mouradian <vahanmouradian@outlook.com>
2020-07-16 17:18:06 -0700

    some commands

--- pulse_counter.py
+++ pulse_counter.py
@@ -31,7 +31,7 @@ print("Pulse detection is now live.")
 test="TRUE"
 subprocess.call('echo "${}" | /home/pi/water_logger/pulse_counter.py --args'.format(test), shell=True)
     
-bashCommand = f"test={testing}"
+bashCommand = f"test='123'"
 os.system(bashCommand) 
 
 
