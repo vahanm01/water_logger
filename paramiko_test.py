99065e30d1cee552a4164eb13d70a0a9ba5275ac
Vahan Mouradian <vahanmouradian@outlook.com>
2020-07-22 16:20:55 -0700

    test ssh

--- paramiko_test.py
+++ paramiko_test.py
@@ -15,6 +15,7 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.load_system_host_keys()
 print('loaded client')
 client.connect(hostname, port, username, password)
+print('connection made')
 
 sftp_client = client.open_sftp()
 
