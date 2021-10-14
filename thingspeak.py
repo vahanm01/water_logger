95dad4bee8fdc20fdccef2e21e335d59d5abc701
Vahan Mouradian <vahanmouradian@outlook.com>
2021-10-13 21:23:36 -0700

    thingspeak

---
+++ thingspeak.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import json 
+import RPi.GPIO as GPIO
+import datetime
+
+
+
+
+import httplib
+import urllib
+import time
+
+
+key = "VZ5GULKMWUUFD0S0"  # Put your API Key here
+
+
+FLOW_SENSOR = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.RISING)
+
+while True:
+    
+  if GPIO.event_detected(FLOW_SENSOR)==True:
+     detect=1
+     
+     params = urllib.urlencode({'field1': detect, 'key':key }) 
+     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+     conn = httplib.HTTPConnection("api.thingspeak.com:80")
+     conn.request("POST", "/update", params, headers)
+     conn.close()
+
+     time.sleep(5)
+         
+
+  if GPIO.event_detected(FLOW_SENSOR)==False:
+      
+     params = urllib.urlencode({'field1': 0, 'key':key }) 
+     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+     conn = httplib.HTTPConnection("api.thingspeak.com:80")
+     conn.request("POST", "/update", params, headers)
+     conn.close()    
+     time.sleep(5)     
+     
+     
